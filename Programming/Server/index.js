86d83c3cc5ad9bdebfa83a03b6945fda951cc54d
an1by <mrkillermafia@gmail.com>
2023-03-28 17:18:10 +0300

    Server crash fix

--- Programming/Server/index.js
+++ Programming/Server/index.js
@@ -24,125 +24,133 @@ const server = net.createServer(socket => {
     const address = createAddress(socket);
 
     Logger.info('Подключено устройство с IP: ' + address);
-    socket.write(JSON.stringify({"action": "log_console", "message": "Вы подключены к серверу!"}), 'utf-8');
+    try {
+        socket.write(JSON.stringify({"action": "log_console", "message": "Вы подключены к серверу!"}), 'utf-8');
+    } catch (e) {
+        console.error(e)
+    }
 
     socket.on('data', received => {
-        let data = JSON.parse(received.toString());
-        // console.log(received.toString())
-        switch (data["command"]) {
-            case "log": {
-                console.log(data["message"])
-            }
-            case "set_pos": {
-                pos_red = data["red"]
-                pos_blue = data["blue"]
-                break
-            }
-            case "get": {
-                socket.write(JSON.stringify({
-                    "red": pos_red,
-                    "blue": pos_blue
-                }), 'utf-8')
-                break
-            }
-            case "debug": {
-                console.log(data["message"])
-                break
-            }
-            case "clear": {
-                switch (data["what"]) {
-                    case "all":
-                        raspberry = undefined;
-                        tank_list.forEach(tank => tank.disconnect())
-                        tank_list = []
-                        obstacle_list = []
-                        client_list.forEach(client => client.disconnect())
-                        Logger.success('Данные очищены, клиенты отключены!')
-                        break
-                    case "obstacles":
-                        obstacle_list = []
-                        break
+        try {
+            let data = JSON.parse(received.toString());
+            // console.log(received.toString())
+            switch (data["command"]) {
+                case "log": {
+                    console.log(data["message"])
                 }
-            }
-            case "edit": {
-                switch (data["what"]) {
-                    case "obstacles": {
-                        data["list"].forEach(obs => {
-                            const positions = obs["positions"]
-                            const state = obs["state"]
-                            if (positions[0] && positions[1] && obs["state"]) {
-                                let obstacle = getObstacleWithArray(positions);
-
-                                if (obstacle) {
-                                    if (state == "empty")
-                                        obstacle.delete();
-                                    else 
-                                        obstacle.state = state;
+                case "set_pos": {
+                    pos_red = data["red"]
+                    pos_blue = data["blue"]
+                    break
+                }
+                case "get": {
+                    socket.write(JSON.stringify({
+                        "red": pos_red,
+                        "blue": pos_blue
+                    }), 'utf-8')
+                    break
+                }
+                case "debug": {
+                    console.log(data["message"])
+                    break
+                }
+                case "clear": {
+                    switch (data["what"]) {
+                        case "all":
+                            raspberry = undefined;
+                            tank_list.forEach(tank => tank.disconnect())
+                            tank_list = []
+                            obstacle_list = []
+                            client_list.forEach(client => client.disconnect())
+                            Logger.success('Данные очищены, клиенты отключены!')
+                            break
+                        case "obstacles":
+                            obstacle_list = []
+                            break
+                    }
+                }
+                case "edit": {
+                    switch (data["what"]) {
+                        case "obstacles": {
+                            data["list"].forEach(obs => {
+                                const positions = obs["positions"]
+                                const state = obs["state"]
+                                if (positions[0] && positions[1] && obs["state"]) {
+                                    let obstacle = getObstacleWithArray(positions);
+    
+                                    if (obstacle) {
+                                        if (state == "empty")
+                                            obstacle.delete();
+                                        else 
+                                            obstacle.state = state;
+                                    }
+                                    else new Obstacle(obs[type], positions[0], positions[1]);
                                 }
-                                else new Obstacle(obs[type], positions[0], positions[1]);
-                            }
-                        });
-                    }
-                }
-            }
-            case "init": {
-                switch (data["who"]) {
-                    case "tank": {
-                        if (getWithType("tank").length >= 6)
-                            return
-                        const number = data["number"];
-                        new Tank(number, (number < 3 ? "red" : "blue"), socket);
-                        Logger.success(`Танк №${number} инициализирован. Адрес: ` + address)
-                        break
-                    }
-                    case "obstacle": {
-                        // if (raspberry.address !== address)
-                        //     return;
-                        const positions = data["positions"];
-                        if (!positions[0] || !positions[1])
-                            return;
-                        new Obstacle(positions[0], positions[1]);
-                        break
-                    }
-                    default: {
-                        if (!getWithType(data["who"])) {
-                            new Client(socket, data["who"])
-                            Logger.success(`Клиент ${data["who"]} инициализирован. Адрес: ` + address)
+                            });
                         }
-                        break
                     }
                 }
-                break
-            }
-            case "move": {
-                // if (address !== raspberry.address)
-                //     return;
-                const number = data["number"],
-                    position = data["position"]
-                let tank = getTank(number);
-                if (!tank)
-                    return;
-                if (position.x < 0 || position.y < 0) {
-                    tank.disconnect();
-                    Logger.warning(`Танк №${number} вышел за поле! Отключен.`)
-                    return;
+                case "init": {
+                    switch (data["who"]) {
+                        case "tank": {
+                            if (getWithType("tank").length >= 6)
+                                return
+                            const number = data["number"];
+                            new Tank(number, (number < 3 ? "red" : "blue"), socket);
+                            Logger.success(`Танк №${number} инициализирован. Адрес: ` + address)
+                            break
+                        }
+                        case "obstacle": {
+                            // if (raspberry.address !== address)
+                            //     return;
+                            const positions = data["positions"];
+                            if (!positions[0] || !positions[1])
+                                return;
+                            new Obstacle(positions[0], positions[1]);
+                            break
+                        }
+                        default: {
+                            if (!getWithType(data["who"])) {
+                                new Client(socket, data["who"])
+                                Logger.success(`Клиент ${data["who"]} инициализирован. Адрес: ` + address)
+                            }
+                            break
+                        }
+                    }
+                    break
                 }
-                tank.position.x = position.x;
-                tank.position.y = position.y;
-                break
-            }
-            case "fire": { // От танка
-                const tank = getTank(data["number"]);
-                const target_position = data["position"]
-                if (!tank) {
-                    Logger.error(`Танк с номером ${data["number"]} не найден!`)
-                    return;
+                case "move": {
+                    // if (address !== raspberry.address)
+                    //     return;
+                    const number = data["number"],
+                        position = data["position"]
+                    let tank = getTank(number);
+                    if (!tank)
+                        return;
+                    if (position.x < 0 || position.y < 0) {
+                        tank.disconnect();
+                        Logger.warning(`Танк №${number} вышел за поле! Отключен.`)
+                        return;
+                    }
+                    tank.position.x = position.x;
+                    tank.position.y = position.y;
+                    break
                 }
-                const result = tank.fire(target_position)
-                Logger.info(`Танк №${tank.number} выстрелил! ${result.message}`)
-                socket.write(JSON.stringify(result), 'utf-8') // fire_feedback
-                break
+                case "fire": { // От танка
+                    const tank = getTank(data["number"]);
+                    const target_position = data["position"]
+                    if (!tank) {
+                        Logger.error(`Танк с номером ${data["number"]} не найден!`)
+                        return;
+                    }
+                    const result = tank.fire(target_position)
+                    Logger.info(`Танк №${tank.number} выстрелил! ${result.message}`)
+                    socket.write(JSON.stringify(result), 'utf-8') // fire_feedback
+                    break
+                }
             }
+        } catch (e) {
+            console.error(e)
         }
     });
 
